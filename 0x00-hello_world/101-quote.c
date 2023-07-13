1bbc3dec5119f6a70dea9367e505b67da693dc32
Toluwalashe <asheogunleye02@gmail.com>
2023-07-13 07:15:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19, followed by a newline, to the standard error.
+ *              It uses the write function to write the message to the STDERR_FILENO.
+ *              The program returns 1 to indicate failure.
+ *
+ * Return: Always EXIT_FAILURE (1)
+ */
+
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+	return EXIT_FAILURE;
+}
