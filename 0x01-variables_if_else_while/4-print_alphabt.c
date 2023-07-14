8b308f41baf217b1a824ae8177da5afca3480edd
Toluwalashe <asheogunleye02@gmail.com>
2023-07-14 13:54:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints the lowercase alphabet excluding 'q' and 'e', followed by a new line,
+ * Description: Prints the lowercase alphabet excluding 'q' and 'e',
  *              using putchar.
  *
  * Return: Always 0 (Success)
