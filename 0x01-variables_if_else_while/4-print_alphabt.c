82725af78c5807ed572486b9c835257d8117a70a
Toluwalashe <asheogunleye02@gmail.com>
2023-07-14 13:50:34 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,18 +3,23 @@
 /**
  * main - Entry point
  *
- * Description: Prints the uppercase alphabet using putchar.
+ * Description: Prints the lowercase alphabet excluding 'q' and 'e', followed by a new line,
+ *              using putchar.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char letter;
-	for (letter = "A": letter <= "Z"; letter++)
-		putchar(letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
 
 	putchar('\n');
 
 	return (0);
+}
 
