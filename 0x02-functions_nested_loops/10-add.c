9cf1a5de2e38e91621d5f7ed836cac9c7690cc29
Toluwalashe <asheogunleye02@gmail.com>
2023-07-18 07:56:35 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,17 @@
-/*
- * add - Add two integers and return thr result.
- * Description - a function that adds two integers 
- * 		and returns the result.
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Description: This function takes two integers, `a` and `b`, as input
+ *              and returns their sum.
+ *
+ * Return: The sum of the two integers
  */
 
 int add(int x, int y)
 {
 	int result = x + y;
+
 	return (result);
 }
