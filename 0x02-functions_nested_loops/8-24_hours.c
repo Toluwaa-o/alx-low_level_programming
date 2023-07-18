a76435f646e068ce5d72a6f0d9efb2172d2c2dd0
Toluwalashe <asheogunleye02@gmail.com>
2023-07-18 07:38:40 -0700

    v

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer.
- * Description - a function that prints every minute of the 
- * 		day of Jack Bauer, starting from 00:00 to 23:59.
+ * Description - a function that prints every minute of the
+ * day of Jack Bauer, starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
