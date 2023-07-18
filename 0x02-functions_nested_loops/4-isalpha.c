05d9006e4803cb920497a8815a765b0e2d6e20f5
Toluwalashe <asheogunleye02@gmail.com>
2023-07-18 06:55:44 -0700

    /**  * _isalpha - Checks if a character is an alphabetic letter.  *  * @c: The character to be checked.  *  * Return: 1 if the character is an alphabetic letter, 0 otherwise.  *  * Description: This function takes a character as input and checks if it is  *              an alphabetic letter. It returns 1 if the character is a  *              letter (either lowercase or uppercase), and 0 otherwise.  */

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,18 @@
-/*
- * _isalpha - checks if c is a letter, lowercase or uppercase.
- * @c - is the character to be checked.
- * Description - a function that checks for alphabetic character.
+/**
+ * _isalpha - Checks if a character is an alphabetic letter.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is an alphabetic letter, 0 otherwise.
+ *
+ * Description: This function takes a character as input and checks if it is
+ *              an alphabetic letter. It returns 1 if the character is a
+ *              letter (either lowercase or uppercase), and 0 otherwise.
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z' || c >= 'a' || c <= 'z')
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
 	else
 		return (0);
