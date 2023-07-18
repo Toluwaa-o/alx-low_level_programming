7f475b685b9c38a491570b39cf86a0b7fde97185
Toluwalashe <asheogunleye02@gmail.com>
2023-07-18 08:05:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,8 +3,8 @@
 /**
  * print_times_table - Prints the n times table starting from 0
  * @n: The number for which the times table is printed
- * Description - a function that prints the n times table, 
- * 		starting with 0.
+ * Description - a function that prints the n times table,
+ * starting with 0.
  */
 
 void print_times_table(int n)
@@ -21,7 +21,7 @@ void print_times_table(int n)
 			result = i * j;
 
 			if (j != 0)
-				printf(",");
+				printf(",  ");
 
 			if (result < 10)
 				printf(" ");
