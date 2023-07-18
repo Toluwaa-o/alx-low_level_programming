686da8d456e8bd609224d30a85c53de6d4f5afab
Toluwalashe <asheogunleye02@gmail.com>
2023-07-18 05:52:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
  * Description: This function prints the lowercase alphabet from 'a' to 'z'
  *              ten times, with each set of alphabets separated by a comma
